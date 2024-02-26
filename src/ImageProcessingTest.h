340e94845d674c812967c7e5030130f6b100cdc0
@users.noreply.github.com <>
2024-02-26 12:35:09 +0000

    Apply clang-format changes

--- src/ImageProcessingTest.h
+++ src/ImageProcessingTest.h
@@ -1,22 +1,21 @@
 #pragma once
-#include <filesystem>
-
-#include <opencv2/opencv.hpp>
 #include <gtest/gtest.h>
+
+#include <filesystem>
+#include <opencv2/opencv.hpp>
+
 #include "Utils/TimerCpu.h"
 
-class ImageProcessingTest : public ::testing::Test
-{
-protected:
-	void SetUp() override;
-	void TearDown() override;
-	cv::Mat readAssetsImage() const;
-	const std::string& getAssetImagePath() const;
-	std::string getCurrentTestName() const;
-	std::string getOutputDir() const;
-	std::shared_ptr<TimerCpu> timerCpu;
+class ImageProcessingTest : public ::testing::Test {
+ protected:
+  void SetUp() override;
+  void TearDown() override;
+  cv::Mat readAssetsImage() const;
+  const std::string& getAssetImagePath() const;
+  std::string getCurrentTestName() const;
+  std::string getOutputDir() const;
+  std::shared_ptr<TimerCpu> timerCpu;
 
-private:
-	const std::string assetImagePath = "assets\\imori.jpg";
+ private:
+  const std::string assetImagePath = "assets\\imori.jpg";
 };
-
--- src/tutorial/tutorial.cpp
+++ src/tutorial/tutorial.cpp
@@ -1,32 +1,33 @@
 #include "../ImageProcessingTest.h"
 
 TEST_F(ImageProcessingTest, Tutorial) {
-	cv::Mat image = readAssetsImage();
+  cv::Mat image = readAssetsImage();
 
-	timerCpu->start();
+  timerCpu->start();
 
-	int width = image.rows;
-	int height = image.cols;
+  int width = image.rows;
+  int height = image.cols;
 
-	cv::Mat out = image.clone();
+  cv::Mat out = image.clone();
 
-	for (int i = 0; i < width / 2; i++) {
-		for (int j = 0; j < height / 2; j++) {
-			unsigned char tmp = out.at<cv::Vec3b>(j, i)[0];
-			out.at<cv::Vec3b>(j, i)[0] = out.at<cv::Vec3b>(j, i)[2];
-			out.at<cv::Vec3b>(j, i)[2] = tmp;
-		}
-	}
-	timerCpu->stop();
+  for (int i = 0; i < width / 2; i++) {
+    for (int j = 0; j < height / 2; j++) {
+      unsigned char tmp = out.at<cv::Vec3b>(j, i)[0];
+      out.at<cv::Vec3b>(j, i)[0] = out.at<cv::Vec3b>(j, i)[2];
+      out.at<cv::Vec3b>(j, i)[2] = tmp;
+    }
+  }
+  timerCpu->stop();
 
-	std::cout << std::format("[{}] CPU time: {:.2f} ms\n", getCurrentTestName(), timerCpu->elapsedMilliseconds());
+  std::cout << std::format("[{}] CPU time: {:.2f} ms\n", getCurrentTestName(),
+                           timerCpu->elapsedMilliseconds());
 
-	std::string outPath = std::format("{}\\out.png", getOutputDir());
-	cv::imwrite(outPath, out);
+  std::string outPath = std::format("{}\\out.png", getOutputDir());
+  cv::imwrite(outPath, out);
 
-	// cv::imshow("sample", out);
-	// cv::waitKey(0);
-	// cv::destroyAllWindows();
+  // cv::imshow("sample", out);
+  // cv::waitKey(0);
+  // cv::destroyAllWindows();
 
-	SUCCEED();
+  SUCCEED();
 }
