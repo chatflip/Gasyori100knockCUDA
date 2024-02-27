1e8a9dc219b14d872e4eebd686c83df3a7869a67
chatflip <oo.chat.flip@gmail.com>
2024-02-28 02:20:50 +0900

    Apply clang-format

--- src/ImageProcessingTest.cpp
+++ src/ImageProcessingTest.cpp
@@ -1,30 +1,26 @@
 #include "ImageProcessingTest.h"
 
-void ImageProcessingTest::SetUp() {
-	timerCpu = std::make_shared<TimerCpu>();
-}
+void ImageProcessingTest::SetUp() { timerCpu = std::make_shared<TimerCpu>(); }
 
-void ImageProcessingTest::TearDown() {
-	timerCpu->reset();
-}
+void ImageProcessingTest::TearDown() { timerCpu->reset(); }
 
 const std::string& ImageProcessingTest::getAssetImagePath() const {
-	return assetImagePath;
+  return assetImagePath;
 };
 
 cv::Mat ImageProcessingTest::readAssetsImage() const {
-	cv::Mat image = cv::imread(getAssetImagePath(), cv::IMREAD_COLOR);
-	return image.clone();
+  cv::Mat image = cv::imread(getAssetImagePath(), cv::IMREAD_COLOR);
+  return image.clone();
 };
 
 std::string ImageProcessingTest::getCurrentTestName() const {
-	const ::testing::TestInfo* const testInfo =
-		::testing::UnitTest::GetInstance()->current_test_info();
-	return testInfo->name();
+  const ::testing::TestInfo* const testInfo =
+      ::testing::UnitTest::GetInstance()->current_test_info();
+  return testInfo->name();
 };
 
 std::string ImageProcessingTest::getOutputDir() const {
-	std::string outputDir = std::format("output\\{}", getCurrentTestName());
-	std::filesystem::create_directories(outputDir);
-	return outputDir;
+  std::string outputDir = std::format("output\\{}", getCurrentTestName());
+  std::filesystem::create_directories(outputDir);
+  return outputDir;
 };
--- src/Utils/TimerCpu.cpp
+++ src/Utils/TimerCpu.cpp
@@ -1,25 +1,22 @@
 #include "TimerCpu.h"
 
 TimerCpu::TimerCpu() {
-	if (!QueryPerformanceFrequency(&frequency)) {
-		std::cerr << "High precision counters are not supported by this system" << std::endl;
-	}
+  if (!QueryPerformanceFrequency(&frequency)) {
+    std::cerr << "High precision counters are not supported by this system"
+              << std::endl;
+  }
 }
 
 void TimerCpu::reset() {
-	startTime.QuadPart = 0;
-	endTime.QuadPart = 0;
+  startTime.QuadPart = 0;
+  endTime.QuadPart = 0;
 }
+void TimerCpu::start() { QueryPerformanceCounter(&startTime); }
 
-void TimerCpu::start() {
-	QueryPerformanceCounter(&startTime);
-}
-
-void TimerCpu::stop() {
-	QueryPerformanceCounter(&endTime);
-}
+void TimerCpu::stop() { QueryPerformanceCounter(&endTime); }
 
 double TimerCpu::elapsedMilliseconds() const {
-	double interval = static_cast<double>(endTime.QuadPart - startTime.QuadPart) / static_cast<double>(frequency.QuadPart);
-	return 1e3 * interval;
+  double interval = static_cast<double>(endTime.QuadPart - startTime.QuadPart) /
+                    static_cast<double>(frequency.QuadPart);
+  return 1e3 * interval;
 }
