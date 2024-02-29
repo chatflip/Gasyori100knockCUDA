03738e9260e7bc9ceee03f9433f56c8bca44c5c2
chatflip <oo.chat.flip@gmail.com>
2024-03-01 04:21:55 +0900

    fast impl(in-place)

--- src/Question_01/execute_channel_swap.cpp
+++ src/Question_01/execute_channel_swap.cpp
@@ -7,18 +7,20 @@ cv::Mat MakeQ1desiredMat(cv::Mat image) {
 }
 
 cv::Mat channelSwapCpu(cv::Mat image) {
-  cv::Mat result = cv::Mat::zeros(image.size(), image.type());
+  // in-place channel swap
   int width = image.cols;
   int height = image.rows;
 
 #pragma omp parallel for
   for (int j = 0; j < height; j++) {
     for (int i = 0; i < width; i++) {
-      cv::Vec3b pixel = image.at<cv::Vec3b>(j, i);
-      result.at<cv::Vec3b>(j, i) = cv::Vec3b(pixel[2], pixel[1], pixel[0]);
+      cv::Vec3b *src = image.ptr<cv::Vec3b>(j, i);
+      uchar tmp = src[0][2];
+      src[0][2] = src[0][0];
+      src[0][0] = tmp;
     }
   }
-  return result;
+  return image;
 }
 
 TEST_F(ImageProcessingTest, Question_01_CPU) {
@@ -31,6 +33,9 @@ TEST_F(ImageProcessingTest, Question_01_CPU) {
 
   std::cout << std::format("[{}] CPU time: {:.2f} ms\n", getCurrentTestName(),
                            timerCpu->elapsedMilliseconds());
+
+  std::string outPath = std::format("{}\\question_01_cpu.png", getOutputDir());
+  cv::imwrite(outPath, resultCpu);
 
   MatCompareResult compareResult = compareMat(resultCpu, desiredImage);
   EXPECT_EQ(compareResult, MatCompareResult::kMatch);
