a3aa5ec0d338ad07b1b2e9c909435070b0c400f2
chatflip <oo.chat.flip@gmail.com>
2024-04-21 04:50:23 +0900

    Added detailed message when a test fails.

--- src/ImageProcessingTest.cpp
+++ src/ImageProcessingTest.cpp
@@ -35,27 +35,83 @@ std::string ImageProcessingTest::getGtestLogDir() const {
   return outputDir;
 };
 
-MatCompareResult ImageProcessingTest::compareMat(const cv::Mat& actual,
-                                                 const cv::Mat& desired) const {
-  using enum MatCompareResult;
-  if (actual.size() != desired.size()) {
-    return kSizeMismatch;
-  }
-  if (actual.type() != desired.type()) {
-    return kTypeMismatch;
-  }
-  if (actual.channels() != desired.channels()) {
-    return kChannelMismatch;
-  }
+std::string ImageProcessingTest::formatSizeMismatchMessage(
+    const cv::Mat& actual, const cv::Mat& expected) const {
+  cv::Size AcutalSize = actual.size();
+  cv::Size ExpectedSize = expected.size();
+  return std::format(
+      "Mat sizes are not equal\n"
+      "Actual:\twidth:{} height:{}\n"
+      "Expected:\twidth:{} height:{}",
+      AcutalSize.width, AcutalSize.height, ExpectedSize.width,
+      ExpectedSize.height);
+}
+
+std::string ImageProcessingTest::formatTypeMismatchMessage(
+    const cv::Mat& actual, const cv::Mat& expected) const {
+  return std::format(
+      "Mat types are not equal\n"
+      "Actual:\t{}\n"
+      "Expected:\t{}",
+      actual.type(), expected.type());
+}
+
+std::string ImageProcessingTest::formatChannelsMismatchMessage(
+    const cv::Mat& actual, const cv::Mat& expected) const {
+  return std::format(
+      "Mat num channels are not equal\n"
+      "Actual:\t{}\n"
+      "Expected:\t{}",
+      actual.channels(), expected.channels());
+}
+
+std::string ImageProcessingTest::formatDepthMismatchMessage(
+    const cv::Mat& actual, const cv::Mat& expected) const {
+  return std::format(
+      "Mat depth are not equal\n"
+      "Actual:\t{}\n"
+      "Expected:\t{}",
+      actual.depth(), expected.depth());
+}
+
+std::string ImageProcessingTest::formatContentMismatchMessage(
+    const cv::Mat& actual, const cv::Mat& expected) const {
+  cv::Mat diff;
+  cv::absdiff(expected, actual, diff);
+  int numDiffPixels = cv::countNonZero(diff);
+  double minDiff, maxDiff;
+  cv::minMaxLoc(diff, &minDiff, &maxDiff);
+  double avgDiff = cv::mean(diff)[0];
+  return std::format(
+      "Mat pixels are not equal\n"
+      "Number of pixels with different intensities:\t{}\n"
+      "Minimum intensity difference:\t{}\n"
+      "Maximum intensity difference:\t{}\n"
+      "Average intensity difference:\t{:.2f}",
+      numDiffPixels, minDiff, maxDiff, avgDiff);
+}
+
+void ImageProcessingTest::compareMat(const cv::Mat& actual,
+                                     const cv::Mat& expected) const {
+  EXPECT_TRUE(actual.size() == expected.size())
+      << formatSizeMismatchMessage(actual, expected);
+
+  EXPECT_TRUE(actual.type() == expected.type())
+      << formatTypeMismatchMessage(actual, expected);
+  EXPECT_TRUE(actual.channels() == expected.channels())
+      << formatChannelsMismatchMessage(actual, expected);
+  EXPECT_TRUE(actual.depth() == expected.depth())
+      << formatDepthMismatchMessage(actual, expected);
+
   cv::Mat diff;
   std::vector<cv::Mat> diffChannels;
-  cv::absdiff(actual, desired, diff);
+  cv::absdiff(actual, expected, diff);
   cv::split(diff, diffChannels);
 
   for (const auto& channel : diffChannels) {
     if (cv::countNonZero(channel) != 0) {
-      return kContentMismatch;
+      FAIL() << formatContentMismatchMessage(actual, expected);
     }
   }
-  return kMatch;
+  SUCCEED();
 };
