1a6124d4bd11b329e34a6d0c015ffcc6e7d31dc5
chatflip <oo.chat.flip@gmail.com>
2024-04-21 15:53:30 +0900

    add cpu impl

---
+++ src/Question_02/Question_02.cpp
@@ -0,0 +1,115 @@
+#include "../ImageProcessingTest.hpp"
+#include "bgr2gray.cuh"
+
+namespace {
+int numQuestions = 2;
+double thrMaxAbsDiff = 1;
+double thrDiffPixelsPercent = 0.01;
+
+std::string logDir = ImageProcessingTest::getQuestionLogDir(numQuestions);
+cv::Mat MakeQ2desiredMat(cv::Mat image) {
+  cv::Mat referenceImage = image.clone();
+  cv::cvtColor(referenceImage, referenceImage, cv::COLOR_BGR2GRAY);
+  return referenceImage;
+}
+
+}  // namespace
+TEST_F(ImageProcessingTest, Question_02_cpu) {
+  cv::Mat desiredImage = MakeQ2desiredMat(inputImage);
+  std::shared_ptr<TimerCpu> cpuTimer = std::make_shared<TimerCpu>();
+
+  cpuTimer->start(actualProcessTimeName);
+  cv::Mat resultCpu = bgr2grayCpu(inputImage, cpuTimer);
+  cpuTimer->stop(actualProcessTimeName);
+  cpuTimer->recordAll();
+  float elapsedTime = cpuTimer->getRecord(actualProcessTimeName);
+  cpuTimer->popRecord(actualProcessTimeName);
+
+  std::string header = cpuTimer->createHeader(getCurrentTestName());
+  std::string footer = cpuTimer->createFooter(elapsedTime);
+  std::string logPath = std::format("{}\\{}.log", logDir, getCurrentTestName());
+  cpuTimer->writeToFile(logPath, header, footer);
+  cpuTimer->print(header, footer);
+
+  compareMatAlmostEqual(resultCpu, desiredImage, thrMaxAbsDiff,
+                        thrDiffPixelsPercent);
+}
+
+/*
+
+TEST_F(ImageProcessingTest, Question_01_gpu) {
+  cv::Mat desiredImage = MakeQ1desiredMat(inputImage);
+  std::shared_ptr<TimerCpu> cpuTimer = std::make_shared<TimerCpu>();
+  std::shared_ptr<TimerGpu> gpuTimer = std::make_shared<TimerGpu>();
+
+  cpuTimer->start(actualProcessTimeName);
+  int numStreams = 8;
+  cv::Mat resultGpu = bgr2rgbGpuMultiStream(
+      inputImage, numStreams, resourceManager, cpuTimer, gpuTimer);
+  cpuTimer->stop(actualProcessTimeName);
+  cpuTimer->recordAll();
+  float elapsedTime = cpuTimer->getRecord(actualProcessTimeName);
+  cpuTimer->popRecord(actualProcessTimeName);
+  gpuTimer->recordAll();
+  gpuTimer->mergeRecords(*cpuTimer);
+
+  std::string header = gpuTimer->createHeader(getCurrentTestName());
+  std::string footer = gpuTimer->createFooter(elapsedTime);
+  std::string logPath = std::format("{}\\{}.log", logDir, getCurrentTestName());
+  gpuTimer->writeToFile(logPath, header, footer);
+  gpuTimer->print(header, footer);
+
+  MatCompareResult compareResult = compareMat(resultGpu, desiredImage);
+  EXPECT_EQ(compareResult, MatCompareResult::kMatch);
+}
+
+TEST_F(ImageProcessingTest, Question_01_gpu_thrust) {
+  cv::Mat desiredImage = MakeQ1desiredMat(inputImage);
+  std::shared_ptr<TimerCpu> cpuTimer = std::make_shared<TimerCpu>();
+  std::shared_ptr<TimerGpu> gpuTimer = std::make_shared<TimerGpu>();
+
+  cpuTimer->start(actualProcessTimeName);
+  cv::Mat resultGpu =
+      bgr2rgbGpuThrust(inputImage, resourceManager, cpuTimer, gpuTimer);
+  cpuTimer->stop(actualProcessTimeName);
+  cpuTimer->recordAll();
+  float elapsedTime = cpuTimer->getRecord(actualProcessTimeName);
+  cpuTimer->popRecord(actualProcessTimeName);
+  gpuTimer->recordAll();
+  gpuTimer->mergeRecords(*cpuTimer);
+
+  std::string header = gpuTimer->createHeader(getCurrentTestName());
+  std::string footer = gpuTimer->createFooter(elapsedTime);
+  std::string logPath = std::format("{}\\{}.log", logDir, getCurrentTestName());
+  gpuTimer->writeToFile(logPath, header, footer);
+  gpuTimer->print(header, footer);
+
+  MatCompareResult compareResult = compareMat(resultGpu, desiredImage);
+  EXPECT_EQ(compareResult, MatCompareResult::kMatch);
+}
+
+TEST_F(ImageProcessingTest, Question_01_gpu_texture) {
+  cv::Mat desiredImage = MakeQ1desiredMat(inputImage);
+  std::shared_ptr<TimerCpu> cpuTimer = std::make_shared<TimerCpu>();
+  std::shared_ptr<TimerGpu> gpuTimer = std::make_shared<TimerGpu>();
+
+  cpuTimer->start(actualProcessTimeName);
+  cv::Mat resultGpu =
+      bgr2rgbGpuTexture(inputImage, resourceManager, cpuTimer, gpuTimer);
+  cpuTimer->stop(actualProcessTimeName);
+  cpuTimer->recordAll();
+  float elapsedTime = cpuTimer->getRecord(actualProcessTimeName);
+  cpuTimer->popRecord(actualProcessTimeName);
+  gpuTimer->recordAll();
+  gpuTimer->mergeRecords(*cpuTimer);
+
+  std::string header = gpuTimer->createHeader(getCurrentTestName());
+  std::string footer = gpuTimer->createFooter(elapsedTime);
+  std::string logPath = std::format("{}\\{}.log", logDir, getCurrentTestName());
+  gpuTimer->writeToFile(logPath, header, footer);
+  gpuTimer->print(header, footer);
+
+  MatCompareResult compareResult = compareMat(resultGpu, desiredImage);
+  EXPECT_EQ(compareResult, MatCompareResult::kMatch);
+}
+   */
---
+++ src/Question_02/bgr2gray.cuh
@@ -0,0 +1,45 @@
+#include <cuda_runtime.h>
+#include <omp.h>
+#include <thrust/copy.h>
+#include <thrust/device_vector.h>
+#include <thrust/system/cuda/execution_policy.h>
+
+#include <opencv2/opencv.hpp>
+
+#include "../Utils/CudaResourceManager.hpp"
+#include "../Utils/TimerCpu.hpp"
+#include "../Utils/TimerGpu.hpp"
+
+/*
+__global__ void bgr2rgbKernel(uchar* input, uchar* output, int width,
+                              int height);
+
+__global__ void bgr2rgbMultiStreamKernel(uchar* input, uchar* output, int width,
+                                         int height, int streamIdx,
+                                         int numStreams);
+
+__global__ void bgr2rgbInplaceKernel(uchar* input, int width, int height);
+
+__global__ void bgr2rgbTextureKernel(cudaTextureObject_t texObj, uchar4* output,
+                                     int width, int height);
+
+cv::Mat bgr2rgbCpuInplace(cv::Mat image, std::shared_ptr<TimerCpu> timer);
+
+cv::Mat bgr2rgbGpuMultiStream(
+    cv::Mat image, int numStreams,
+    std::shared_ptr<CudaResourceManager> resourceManager,
+    std::shared_ptr<TimerCpu> cpuTimer, std::shared_ptr<TimerGpu> gpuTimer);
+
+cv::Mat bgr2rgbGpuThrust(cv::Mat image,
+                         std::shared_ptr<CudaResourceManager> resourceManager,
+                         std::shared_ptr<TimerCpu> cpuTimer,
+                         std::shared_ptr<TimerGpu> gpuTimer);
+
+cv::Mat bgr2rgbGpuTexture(cv::Mat image,
+                          std::shared_ptr<CudaResourceManager> resourceManager,
+                          std::shared_ptr<TimerCpu> cpuTimer,
+                          std::shared_ptr<TimerGpu> gpuTimer);
+
+                          */
+
+cv::Mat bgr2grayCpu(cv::Mat image, std::shared_ptr<TimerCpu> timer);
