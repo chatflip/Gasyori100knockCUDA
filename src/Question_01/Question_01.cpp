1d828ee8594ffbaaf28a6ce9f8f9dbd3fafa7a89
chatflip <oo.chat.flip@gmail.com>
2024-04-21 20:23:15 +0900

    separate cpu/cuda impl

--- src/Question_01/Question_01.cpp
+++ src/Question_01/Question_01.cpp
@@ -12,6 +12,27 @@ cv::Mat MakeQ1desiredMat(cv::Mat image) {
 }
 }  // namespace
 
+cv::Mat bgr2rgbCpuInplace(cv::Mat image, std::shared_ptr<TimerCpu> timer) {
+  timer->start("Allocate Result Memory");
+  cv::Mat result = image.clone();
+  timer->stop("Allocate Result Memory");
+
+  timer->start("Execute Image Processing");
+  int width = image.cols;
+  int height = image.rows;
+#pragma omp parallel for collapse(2)
+  for (int j = 0; j < height; j++) {
+    for (int i = 0; i < width; i++) {
+      cv::Vec3b* src = result.ptr<cv::Vec3b>(j, i);
+      uchar tmp = src[0][2];
+      src[0][2] = src[0][0];
+      src[0][0] = tmp;
+    }
+  }
+  timer->stop("Execute Image Processing");
+  return result;
+}
+
 TEST_F(ImageProcessingTest, Question_01_cpu) {
   cv::Mat desiredImage = MakeQ1desiredMat(inputImage);
   std::shared_ptr<TimerCpu> cpuTimer = std::make_shared<TimerCpu>();
--- src/Question_01/bgr2rgb.cu
+++ src/Question_01/bgr2rgb.cu
@@ -21,8 +21,7 @@ __global__ void bgr2rgbKernel(uchar* input, uchar* output, int width,
 }
 
 __global__ void bgr2rgbMultiStreamKernel(uchar* input, uchar* output, int width,
-                                         int height, int streamIdx,
-                                         int numStreams) {
+                                         int height) {
   int x = blockIdx.x * blockDim.x + threadIdx.x;
   int y = blockIdx.y * blockDim.y + threadIdx.y;
   if (x >= width || y >= height) return;
@@ -65,27 +64,6 @@ __global__ void bgr2rgbTextureKernel(cudaTextureObject_t texObj, uchar4* output,
   rgbaValue.w = bgraValue.w;  // A（Alpha channel remains unchanged.）
 
   output[y * width + x] = rgbaValue;
-}
-
-cv::Mat bgr2rgbCpuInplace(cv::Mat image, std::shared_ptr<TimerCpu> timer) {
-  timer->start("Allocate Result Memory");
-  cv::Mat result = image.clone();
-  timer->stop("Allocate Result Memory");
-
-  timer->start("Execute Image Processing");
-  int width = image.cols;
-  int height = image.rows;
-#pragma omp parallel for collapse(2)
-  for (int j = 0; j < height; j++) {
-    for (int i = 0; i < width; i++) {
-      cv::Vec3b* src = result.ptr<cv::Vec3b>(j, i);
-      uchar tmp = src[0][2];
-      src[0][2] = src[0][0];
-      src[0][0] = tmp;
-    }
-  }
-  timer->stop("Execute Image Processing");
-  return result;
 }
 
 cv::Mat bgr2rgbGpuMultiStream(
@@ -141,8 +119,8 @@ cv::Mat bgr2rgbGpuMultiStream(
     dim3 gridSize((width + blockSize.x - 1) / blockSize.x,
                   (heightPerStream + blockSize.y - 1) / blockSize.y);
     bgr2rgbMultiStreamKernel<<<gridSize, blockSize, sharedMemSizeByte,
-                               streams.at(i)>>>(
-        d_inputs.at(i), d_outputs.at(i), width, heightPerStream, i, numStreams);
+                               streams.at(i)>>>(d_inputs.at(i), d_outputs.at(i),
+                                                width, heightPerStream);
     gpuTimer->stop("Async Execute Cuda Kernel", streams.at(i), false);
   }
 
@@ -160,6 +138,7 @@ cv::Mat bgr2rgbGpuMultiStream(
   for (int i = 0; i < numStreams; i++) {
     gpuTimer->start("Async Deallocate Device Memory", streams.at(i));
     cudaFreeAsync(d_inputs.at(i), streams.at(i));
+    cudaFreeAsync(d_outputs.at(i), streams.at(i));
     gpuTimer->stop("Async Deallocate Device Memory", streams.at(i), false);
   }
 
@@ -210,7 +189,6 @@ cv::Mat bgr2rgbGpuThrust(cv::Mat image,
 
   gpuTimer->start("Wait For GPU Execution");
   cudaStreamSynchronize(streams.at(0));
-  cudaStreamDestroy(streams.at(0));
   gpuTimer->stop("Wait For GPU Execution");
 
   return result;
@@ -288,7 +266,6 @@ cv::Mat bgr2rgbGpuTexture(cv::Mat image,
   gpuTimer->start("Wait For GPU Execution");
   cudaStreamSynchronize(streams.at(0));
   cudaDeviceSynchronize();
-  cudaStreamDestroy(streams.at(0));
   gpuTimer->stop("Wait For GPU Execution");
 
   gpuTimer->start("Restore Output Image");
