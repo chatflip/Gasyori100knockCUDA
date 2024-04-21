bf5a115abe421a0779f8f969434c9525e51c5ac0
chatflip <oo.chat.flip@gmail.com>
2024-04-21 20:23:33 +0900

    add q2 impl

--- src/Question_02/Question_02.cpp
+++ src/Question_02/Question_02.cpp
@@ -13,6 +13,30 @@ cv::Mat MakeQ2desiredMat(cv::Mat image) {
   return referenceImage;
 }
 
+cv::Mat bgr2grayCpu(cv::Mat image, std::shared_ptr<TimerCpu> timer) {
+  timer->start("Allocate Result Memory");
+  int width = image.cols;
+  int height = image.rows;
+  cv::Mat result(height, width, CV_8UC1);
+  timer->stop("Allocate Result Memory");
+
+  timer->start("Execute Image Processing");
+#pragma omp parallel for collapse(2)
+  for (int j = 0; j < height; j++) {
+    for (int i = 0; i < width; i++) {
+      cv::Vec3b* src = image.ptr<cv::Vec3b>(j, i);
+      uchar* dst = result.ptr<uchar>(j, i);
+      // NOTE: This implementation does not strictly match OpenCV because
+      // OpenCV's internal implementation performs calculations by bit shifting
+      uchar gray =
+          round(0.114f * src[0][0] + 0.587f * src[0][1] + 0.299f * src[0][2]);
+      *dst = gray;
+    }
+  }
+  timer->stop("Execute Image Processing");
+  return result;
+}
+
 }  // namespace
 TEST_F(ImageProcessingTest, Question_02_cpu) {
   cv::Mat desiredImage = MakeQ2desiredMat(inputImage);
@@ -35,16 +59,14 @@ TEST_F(ImageProcessingTest, Question_02_cpu) {
                         thrDiffPixelsPercent);
 }
 
-/*
-
-TEST_F(ImageProcessingTest, Question_01_gpu) {
-  cv::Mat desiredImage = MakeQ1desiredMat(inputImage);
+TEST_F(ImageProcessingTest, Question_02_gpu) {
+  cv::Mat desiredImage = MakeQ2desiredMat(inputImage);
   std::shared_ptr<TimerCpu> cpuTimer = std::make_shared<TimerCpu>();
   std::shared_ptr<TimerGpu> gpuTimer = std::make_shared<TimerGpu>();
 
   cpuTimer->start(actualProcessTimeName);
   int numStreams = 8;
-  cv::Mat resultGpu = bgr2rgbGpuMultiStream(
+  cv::Mat resultGpu = bgr2grayGpuMultiStream(
       inputImage, numStreams, resourceManager, cpuTimer, gpuTimer);
   cpuTimer->stop(actualProcessTimeName);
   cpuTimer->recordAll();
@@ -59,57 +81,6 @@ TEST_F(ImageProcessingTest, Question_01_gpu) {
   gpuTimer->writeToFile(logPath, header, footer);
   gpuTimer->print(header, footer);
 
-  MatCompareResult compareResult = compareMat(resultGpu, desiredImage);
-  EXPECT_EQ(compareResult, MatCompareResult::kMatch);
+  compareMatAlmostEqual(resultGpu, desiredImage, thrMaxAbsDiff,
+                        thrDiffPixelsPercent);
 }
-
-TEST_F(ImageProcessingTest, Question_01_gpu_thrust) {
-  cv::Mat desiredImage = MakeQ1desiredMat(inputImage);
-  std::shared_ptr<TimerCpu> cpuTimer = std::make_shared<TimerCpu>();
-  std::shared_ptr<TimerGpu> gpuTimer = std::make_shared<TimerGpu>();
-
-  cpuTimer->start(actualProcessTimeName);
-  cv::Mat resultGpu =
-      bgr2rgbGpuThrust(inputImage, resourceManager, cpuTimer, gpuTimer);
-  cpuTimer->stop(actualProcessTimeName);
-  cpuTimer->recordAll();
-  float elapsedTime = cpuTimer->getRecord(actualProcessTimeName);
-  cpuTimer->popRecord(actualProcessTimeName);
-  gpuTimer->recordAll();
-  gpuTimer->mergeRecords(*cpuTimer);
-
-  std::string header = gpuTimer->createHeader(getCurrentTestName());
-  std::string footer = gpuTimer->createFooter(elapsedTime);
-  std::string logPath = std::format("{}\\{}.log", logDir, getCurrentTestName());
-  gpuTimer->writeToFile(logPath, header, footer);
-  gpuTimer->print(header, footer);
-
-  MatCompareResult compareResult = compareMat(resultGpu, desiredImage);
-  EXPECT_EQ(compareResult, MatCompareResult::kMatch);
-}
-
-TEST_F(ImageProcessingTest, Question_01_gpu_texture) {
-  cv::Mat desiredImage = MakeQ1desiredMat(inputImage);
-  std::shared_ptr<TimerCpu> cpuTimer = std::make_shared<TimerCpu>();
-  std::shared_ptr<TimerGpu> gpuTimer = std::make_shared<TimerGpu>();
-
-  cpuTimer->start(actualProcessTimeName);
-  cv::Mat resultGpu =
-      bgr2rgbGpuTexture(inputImage, resourceManager, cpuTimer, gpuTimer);
-  cpuTimer->stop(actualProcessTimeName);
-  cpuTimer->recordAll();
-  float elapsedTime = cpuTimer->getRecord(actualProcessTimeName);
-  cpuTimer->popRecord(actualProcessTimeName);
-  gpuTimer->recordAll();
-  gpuTimer->mergeRecords(*cpuTimer);
-
-  std::string header = gpuTimer->createHeader(getCurrentTestName());
-  std::string footer = gpuTimer->createFooter(elapsedTime);
-  std::string logPath = std::format("{}\\{}.log", logDir, getCurrentTestName());
-  gpuTimer->writeToFile(logPath, header, footer);
-  gpuTimer->print(header, footer);
-
-  MatCompareResult compareResult = compareMat(resultGpu, desiredImage);
-  EXPECT_EQ(compareResult, MatCompareResult::kMatch);
-}
-   */
