d9ac71971594b981bbdc5bec67da216a38d7b019
chatflip <oo.chat.flip@gmail.com>
2024-05-07 01:18:01 +0900

    add profiler maker

--- src/Utils/CudaResourceManager.hpp
+++ src/Utils/CudaResourceManager.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <cuda_runtime.h>
+#include <nvtx3/nvToolsExt.h>
 
 #include <iostream>
 #include <vector>
@@ -9,9 +10,12 @@ class CudaResourceManager {
   CudaResourceManager();
   ~CudaResourceManager();
   std::vector<cudaStream_t> getStreams(int numStreams = 1);
+  void pushMarker(std::string markerName);
+  void popMarker();
 
  private:
   const int maxStreams = 64;
   std::vector<cudaStream_t> createCudaStreams(int numStreams);
   std::vector<cudaStream_t> streams;
+  bool isMarkered = false;
 };
